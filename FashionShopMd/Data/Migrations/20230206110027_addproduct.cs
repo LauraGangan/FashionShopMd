d02cc576b0e5304d5546e9616f665d6a8f602672
user <user@WIN-MO9Q677QFB7>
2023-02-11 10:02:33 +0200

    Add project files.

---
+++ FashionShopMd/Data/Migrations/20230206110027_addproduct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FashionShopMd.Data.Migrations
+{
+    public partial class addproduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cost = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<int>(type: "int", nullable: false),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OnStok = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ FashionShopMd/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace FashionShopMd.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Image { get; set; }
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public decimal Cost { get; set; }
+
+		public int Discount { get; set; }
+
+		public string Size { get; set; }
+		public string Color { get; set; }
+		public string OnStok { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+		
+
+	}
+}
